359887fe1dcc8327b6272ebd913fb49c91609cbe
apl09 <adrianpastorlopez09@gmail.com>
2023-05-26 18:12:36 +0200

    js

---
+++ fundamentos-de-programacion-bikram-var-bucles-condicionales-master/index.js
@@ -0,0 +1,97 @@
+let variableSinValor;
+
+let booleano1 = true;
+
+let booleano2 = false;
+
+const PI = 3.14;
+
+const TAU = PI*2;
+
+let booleanoAnd = booleano1 || booleano2;
+
+let booleanoNot = !booleano1;
+
+let booleanoMix0 =  booleanoAnd && booleano1 || !(booleano1&&booleano2);
+
+let incrementarDesp = 2;
+resultadoDesp = incrementarDesp++;
+
+let incrementarAntes = 2;
+resultadoAntes = ++incrementarDesp;
+
+
+for (let contarHasta10_2 = 0; 10 == contarHasta10_2 ; contarHasta10_2++);
+
+for (postI = 0;  11== postI ; postI++) {
+    for (postJ = 0; postJ == postI; postJ++) {
+    }    
+      };
+
+
+      let sumaPares = 0
+ for (let i = 0; i == 10; i++) {
+    if(i%2==0)
+  {
+    sumaPares=sumaPares+i
+  }    
+    
+ };
+ 
+ 
+ let variableValorNumerico = 10;
+
+ const MiNombre = 'Adrian';
+
+ const MiNumeroFav = 20;
+
+let booleanoOr = booleano1 || booleano2;
+
+let booleanoMix1 = (booleano1 && TAU/2 == PI) || (variableValorNumerico >= MiNumeroFav);
+
+let seisNoEsNueve = 6 !== 9;
+
+let booleanoMix2 = variableValorNumerico < -(MiNumeroFav * TAU);
+
+let valorSuma = MiNumeroFav + variableValorNumerico;
+
+let valorResta = MiNumeroFav - variableValorNumerico;
+
+let valorMultiplicación = MiNumeroFav * variableValorNumerico;
+
+let valorDivisión = MiNumeroFav/3;
+
+let contarHasta10 = 0
+
+while (contarHasta10 === 10) {
+    contarHasta10 ++;
+    
+  };
+
+  for (preI = 0;  11== preI ; preI++) {
+    for (preJ = 0; preJ == preI; preJ++) {
+    }    
+      };
+
+
+ let sumaImares = 0
+ for (let i = 0; i == 10; i++) {
+    if(i%2!=0)
+  {
+    sumaIares=sumaIares+i
+  }    
+    
+ };
+
+
+
+
+ 
+ 
+
+
+
+
+    
+ 
+
