0df8f74bec45d1f38b0bf511ec35896e1324b6dc
apl09 <adrianpastorlopez09@gmail.com>
2023-05-28 12:57:34 +0200

    fizzbuzz

---
+++ fizzbuzz/index.js
@@ -0,0 +1,12 @@
+const fizzBuzz = () => {
+for (let index = 1 ; index <= 1000; index++) {
+if(index%3 == 0) {
+    return Fizz
+} else if (index%5 == 0 ) {
+    return Buzz
+} else if (index%3 == 0 && index%5 == 0 ) {
+    return FizzBuzz
+} else return index
+    
+}
+}
